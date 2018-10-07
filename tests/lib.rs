9dea7fc6d4921ea1968d607fd4992b5c50ffa9e4
nicohman <nicohman@demenses.net>
2018-10-07 09:14:23 -0700

    Fix accidental commit

--- tests/lib.rs
+++ tests/lib.rs
@@ -5,7 +5,7 @@ use gog::Gog;
 #[test]
 fn get_token() {
     let uri = "auth.gog.com/auth?client_id=46899977096215655&redirect_uri=https://embed.gog.com/on_login_success?origin=client&response_type=code&layout=client2";
-    let token = Token::from_login_code("***REMOVED***").unwrap();
+    let token = Token::from_login_code("").unwrap();
     println!("{:?}",token);
     let gog = Gog::new(token);
     println!("{:?}", gog.get_user_data());
