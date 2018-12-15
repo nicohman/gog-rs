5f410af14f501c58994faaf16bd36340ce2a6a4d
Nico Hickman <nicohman@demenses.net>
2018-12-14 17:31:24 -0800

    Restructure tests to be seperated

--- tests/lib.rs
+++ tests/lib.rs
@@ -1,24 +1,60 @@
 use std::process::Command;
 use std::env::var_os;
+use std::fs::File;
 extern crate gog;
 use gog::token::Token;
 use gog::Gog;
-#[test]
-fn get_token() {
+use std::io::Read;
+fn get_gog() -> Gog {
+    let path = var_os("GOG_TOKEN_PATH").unwrap().into_string().unwrap();
+    let mut token_json = String::new();
+    File::open(path).unwrap().read_to_string(&mut token_json).unwrap();
     let uri = "auth.gog.com/auth?client_id=46899977096215655&redirect_uri=https://embed.gog.com/on_login_success?origin=client&response_type=code&layout=client2";
-    let token = Token::from_login_code(&var_os("GOG_KEY").unwrap().into_string().unwrap()).unwrap();
-    println!("{:?}",token);
+    let token = Token::from_response(&token_json).unwrap();
     let mut gog = Gog::new(token);
-    println!("{:?}", gog.get_user_data().unwrap());
+    gog
+}
+#[test]
+fn get_games() {
+    let gog = get_gog();
+    gog.get_games().unwrap();
+}
+#[test]
+fn get_pub_info() {
+    let gog = get_gog();
     let exp :Vec<String> = vec!["friendStatus".to_string(),"wishlistStatus".to_string(),"blockedStatus".to_string()];
-    println!("{:?}", gog.get_pub_info(49171277422358, exp).unwrap());
-    println!("{:?}", gog.get_games().unwrap());
-    println!("{:?}", gog.get_game_details(1452598881).unwrap());
-    println!("{:?}", gog.wishlist().unwrap());
-    println!("{:?}",gog.add_wishlist(1096313866).unwrap());
-    println!("{:?}:", gog.rm_wishlist(1096313866).unwrap());
-    let exp_game : Vec<String> = vec!["downloads".to_string(),"description".to_string(),"screenshots".to_string(),"related_products".to_string()];
-    println!("{:?}", gog.product(vec![1452598881,1096313866], exp_game).unwrap());
-    println!("{:?}", gog.game_ratings().unwrap());
+    gog.get_pub_info(49171277422358, exp).unwrap();
+}
+#[test]
+fn get_game_details()
+{
+    let gog = get_gog();
+    gog.get_game_details(1452598881).unwrap();
 }
 
+#[test]
+fn wishlist() {
+    let gog = get_gog();
+    gog.wishlist().unwrap();
+}
+#[test]
+fn add_wishlist() {
+    let gog = get_gog();
+    gog.add_wishlist(1096313866).unwrap();
+}
+#[test]
+fn rm_wishlist() {
+    let gog = get_gog();
+    gog.rm_wishlist(1096313866).unwrap();
+}
+#[test]
+fn product() {
+    let exp_game : Vec<String> = vec!["downloads".to_string(),"description".to_string(),"screenshots".to_string(),"related_products".to_string()];
+    let gog = get_gog();
+    gog.product(vec![1452598881,1096313866], exp_game).unwrap();
+}
+#[test]
+fn game_ratings() {
+    let gog = get_gog();
+    gog.game_ratings().unwrap();
+}
